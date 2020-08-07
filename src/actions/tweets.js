7a5a7015b46c0887e824e0af9ef02867c991e530
Francisco Muñoz <padilla.fco@gmail.com>
2020-08-07 17:37:37 +0100

    Liking a Tweet

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,3 +1,6 @@
+import { saveLikeToggle } from '../utils/api'
+
+export const TOGGLE_LIKE = 'TOGGLE_LIKE'
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS'
 
 export function receiveTweets (tweets) {
@@ -6,3 +9,25 @@ export function receiveTweets (tweets) {
     tweets,
   }
 } 
+
+function toggleLike ({ id, authedUser, hasLiked }) {
+  return {
+    type: TOGGLE_LIKE,
+    id,
+    authedUser,
+    hasLiked
+  }
+}
+
+export function handleToggleLike (info) {
+  return (dispatch) => {
+    dispatch(toggleLike(info))
+
+    return saveLikeToggle(info)
+      .catch((e) => {
+        console.warn('Error in handleToggleTweet: ', e)
+        dispatch(toggleLike(info))
+        alert('The was an error liking the tweet. Try again.')
+      })
+  }
+}
