0c6d13ebfdbc5af4beaaaffdd76ecc0af2635628
Francisco Muñoz <padilla.fco@gmail.com>
2020-09-10 18:10:35 +0100

    Fix indentation

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -25,16 +25,16 @@ export default function tweets (state = {}, action) {
         replyingTo = {
           [tweet.replyingTo]: {
             ...state[tweet.replyingTo],
-            replies: state[tweet.replyingTo].replies.concat([tweet.id])    
+           replies: state[tweet.replyingTo].replies.concat([tweet.id])    
+          }
         }
       }
-    }
 
-    return {
-      ...state,
-      [action.tweet.id]: action.tweet,
-      ...replyingTo,
-    }
+      return {
+        ...state,
+        [action.tweet.id]: action.tweet,
+        ...replyingTo,
+      }
     default :
       return state
   }
